91cccf9f0bbd7ab0cc53b237668c93ecdefa0444
Madictian <fredrikbille@hotmail.com>
2020-09-07 11:45:04 +0200

    Initial commit

---
+++ src/Forste.java
@@ -0,0 +1,12 @@
+
+public class Forste {
+    public static void main(String[] args) {
+        // forkortet psvm
+        System.out.println("Hello World!");
+         // forkortet sou
+        System.out.println("I am totally clueless and i do not know how to deal with it...");
+        System.out.println("here we go again");
+        System.out.println("here we go again");
+    }
+
+}
